fe752c15b1daf3f1c977145e998dac19cd8ba96f
romain <rmathieu13@gmail.com>
2019-02-20 09:29:24 +0100

    make navigation bar and display categories in right

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from blog import views
 
 urlpatterns = [
     path('posts/', views.post_list, name='home'),
+    path('posts/<str:category_name>/', views.post_list, name='post-list'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,13 +2,20 @@
 from django.shortcuts import render
 
 from blog.models import Post
+from blog import model_helpers
+from blog import navigation
 
 
-def post_list(request):
-    posts = Post.objects.all()
-
+def post_list(request, category_name=model_helpers.post_category_all.slug()):
+    category, posts = model_helpers.get_category_and_posts(category_name)
+    categories = model_helpers.get_categories()
     context = {
-        'posts', posts,
+        'navigation_item': navigation.navigation_items(navigation.NAV_POSTS),
+        'category': category,
+        'posts': posts,
+        'categories': categories,
     }
 
-    return render(request, 'blog/post_list.html', locals())
+    return render(request, 'blog/post_list.html', context)
+
+
